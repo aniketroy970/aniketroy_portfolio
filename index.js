a744bf912eed8bcc0b4b0e12605d04a43223e6e3
aniketroy970 <aniket.roy@stu.adamasuniversity.ac.in>
2024-07-16 00:56:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,24 +10,12 @@ const port=process.env.PORT || 5500;
 
 app.use(express.static(path.join(__dirname,'public')));
 
-/*mongoose.connect('mongodb+srv://aniketroy:personalwebsite@cluster0.lslotdu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Portfolio_message_db', {
+mongoose.connect('mongodb+srv://aniketroy:aniket123@cluster0.lslotdu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/portfolio_message_db', {
     useNewUrlParser:true,
     useUnifiedTopology:true,
-}).then(()=>{
-    console.log("MongoDB is connected")
-})*/
-try{
-    mongoose.connect('mongodb+srv://aniketroy:aniket123@cluster0.lslotdu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/message', {
-    useNewUrlParser:true,
-    useUnifiedTopology:true,
-    serverSelectionTimeoutMS: 90000,
-    socketTimeoutMS: 45000, 
 }).then(()=>{
     console.log("MongoDB is connected")
 })
-}catch(err){
-    console.log("error to connect");
-}
 const Schema=mongoose.Schema;
 const dataschema=new Schema({
     name:String,
